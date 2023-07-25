488aabee025dd75f6d800fc5510693bcb90695a8
Padmesh624 <padmeshparames2002@gmail.com>
2023-07-25 07:02:33 +0000

    Intial commit

--- dotnetapp/Controllers/UserController.cs
+++ dotnetapp/Controllers/UserController.cs
@@ -32,23 +32,7 @@ namespace User.Controller
                 });
             }
 
-            // [HttpGet("user/getProfile/{ID}")]
-            // public async Task<IActionResult> getProfile(int ID)
-            // {
-            //     var Userprofile = await _context.User.FirstOrDefaultAsync(p => p.ID == ID);
-
-            //     if (Userprofile == null)
-            //     {
-            //         return NotFound(new
-            //         {
-            //             Message = "No user found"
-            //         });
-            //     }
-
-            // return Ok(Userprofile);
-               
-
-           // }
+            
            [HttpGet("user/getProfile")]
             public async Task<IActionResult> getProfiles()
             {
@@ -76,6 +60,7 @@ namespace User.Controller
 
                 if (Userprofile == null)
                 {
+                   
                     return NotFound();
                 }
 
